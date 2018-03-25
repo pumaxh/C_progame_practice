095c1453ef46a204f35085ede5c77c9553bc8734
pumaxh <pumaxh@gmail.com>
2018-03-25 07:50:52 -0700

    Add gtest sample

---
+++ test_files/src/bin/CMakeFiles/sample_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pumaxh/C_program/C_progame_practice/test_files/src/sample1.cc" "/home/pumaxh/C_program/C_progame_practice/test_files/src/bin/CMakeFiles/sample_test.dir/sample1.cc.o"
+  "/home/pumaxh/C_program/C_progame_practice/test_files/src/sample1_unittest.cc" "/home/pumaxh/C_program/C_progame_practice/test_files/src/bin/CMakeFiles/sample_test.dir/sample1_unittest.cc.o"
+  "/home/pumaxh/C_program/C_progame_practice/test_files/src/test_main.cpp" "/home/pumaxh/C_program/C_progame_practice/test_files/src/bin/CMakeFiles/sample_test.dir/test_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pumaxh/C_program/googletest/googletest/include"
+  "/home/pumaxh/C_program/googletest/googletest/samples"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test_files/src/bin/CMakeFiles/sample_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_test.dir/test_main.cpp.o"
+  "CMakeFiles/sample_test.dir/sample1.cc.o"
+  "CMakeFiles/sample_test.dir/sample1_unittest.cc.o"
+  "sample_test.pdb"
+  "sample_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
